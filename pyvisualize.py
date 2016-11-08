1fe8762c5e295d9bcf7dd4cc290b5458bf755b1b
John Anderson <jander43@vols.utk.edu>
2016-11-08 11:45:33 -0500

    Refactored __doc__

--- pyvisualize.py
+++ pyvisualize.py
@@ -1,32 +1,31 @@
 #!/usr/bin/env python
 
 '''
-Copyright 2016 John David Anderson
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
 Author: John D. Anderson
 Email: jander43@vols.utk.edu
-Program: PyVisualize
-
 Description:
     This program allows for quick visuzalition of large
     NetLogo BehaviorSpace data sets.
-
 Attributions:
     martineau, Wed Oct 05 2016, renegade, "Heat map from data
     points in python", Mar 25, 2015 at 22:11.
     http://stackoverflow.com/a/29269645/6926917
+Legal:
+    Copyright 2016 John David Anderson
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+Usage:
+    pyvisualize
 '''
 
 # libraries for GUI/Threads
